7c0e4bde2ee71446c63d143e6a1b2cd33412935e
ellin <ellin@bigc.im>
2023-10-23 22:15:59 +0900

    10/24 DB 연동 api 만들기

--- src/main/java/com/jojoidu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoidu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoidu.book.springboot.domain.posts;
 
+import com.jojoidu.book.springboot.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
